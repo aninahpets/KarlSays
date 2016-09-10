6dca3ed50f33509e3b73b582b164933a46aafbd8
Nina <smulhollandfuchs@gmail.com>
2016-09-10 20:29:27 +0000

    Cleaned up

--- helper_functions.py
+++ helper_functions.py
@@ -1,8 +1,7 @@
 import os
 import pdb
-import googlemaps
 import requests
-from model import User, Venue, Visit, connect_to_db, db
+from model import User, connect_to_db, db
 from random import randrange
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_sqlalchemy import SQLAlchemy

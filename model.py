f48fb87143270e2351f382d3b6d6762c4295df6b
Nina <smulhollandfuchs@gmail.com>
2016-09-10 22:08:34 +0000

    Added parks table

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    username = db.Column(db.String(75), unique=True, nullable=False)
+    username = db.Column(db.String(75), unique=True, nullable=False, primary_key=True)
     password = db.Column(db.String(75), nullable=False)
 
 
@@ -60,6 +60,17 @@ class User(db.Model):
         if 'user_id' in session:
             return True
         return False
+
+
+class Park(db.Model):
+    """Parks in SF."""
+
+    __tablename__ = 'parks'
+
+    park_name = db.Column(db.String, unique=True, nullable=False, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
 
 def connect_to_db(app, db_uri='postgresql:///karlsays'):
     """Connect the database to the Flask app."""
